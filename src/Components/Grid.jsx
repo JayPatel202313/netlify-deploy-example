a63378efdd8ef566fa4edf10289ded73dbc62098
JayPatel202313 <jpatel@pursuit.org>
2024-04-14 16:14:17 -0400

    New

---
+++ src/Components/Grid.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Grid = () => {
+    return (
+        <div className="Grid">
+            <div class="item row0col0 leftBorder"></div>
+            <div class="item row0col1"></div>
+            <div class="item row0col2 rightBorder"></div>
+
+
+
+            <div class="item row1col0 leftBorder"></div>
+            <div class="item row1col1"></div>
+            <div class="item row1col2"></div>
+            <div class="item row1col3 rightBorder"></div>
+
+            <div class="item row2col0 leftBorder"></div>
+            <div class="item row2col1"></div>
+            <div class="item row2col2"></div>
+            <div class="item row2col3 rightBorder"></div>
+
+            <div class="item row3col0 leftBorder"></div>
+            <div class="item row3col1"></div>
+            <div class="item row3col2"></div>
+            <div class="item row3col3 rightBorder"></div>
+        </div>
+    )
+}
+
+export default Grid
